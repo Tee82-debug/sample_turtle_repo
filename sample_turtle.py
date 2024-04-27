3770deb9060ea9beb1ff5dca0776ad6bf106cf8a
Tee82-debug <140274937+Tee82-debug@users.noreply.github.com>
2024-04-27 12:21:40 +0100

    Just added sample turtle file.

---
+++ sample_turtle.py
@@ -0,0 +1,14 @@
+import turtle as tt
+
+
+print('Izu'+33)
+
+for _ in range(4):
+    tt.penup()
+    tt.forward(100)
+    tt.pendown()
+    tt.circle(25)
+    tt.left(90)
+
+tt.hideturtle()
+tt.done()
